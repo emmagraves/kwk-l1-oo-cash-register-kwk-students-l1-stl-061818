87f2aaf7b59c57898d66a8caa6135987e3cafc28
emmagraves <emma.graves09@gmail.com>
2018-06-22 20:15:01 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,6 +1,1 @@
-# Code your cash register here!
-class Amazon_Transaction
-  def initialize(total)
-    @total = "zero"                                                                                                        
-  end
-end
+amazon_transaction
