feb7c344e99553d029bd427c6b35cccf05bea9b7
emmagraves <emma.graves09@gmail.com>
2018-06-22 20:23:58 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,6 +1,18 @@
 class user 
-  def initailize(username, pasword)
+ def initailize(username, pasword)
     @username = username
     @pasword = pasword
   end 
-  
+end 
+
+def username
+  @username
+end 
+
+def pasword
+  @pasword
+end 
+
+class amazon
+  def  (initailize)
+    
